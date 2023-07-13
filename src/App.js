33cb45098ee806dbdd1eb746dc88a12f8bbc6dd0
Rosaverde <rosaverde.ag@gmail.com>
2023-07-13 23:38:22 +0000

    Adding welcome page state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 import Welcome from "./components/Welcome";
+import Card from "./components/Card";
 
 export default function App()
 {
+    const [welcome, setWelcome] = React.useState(true)
+
+    function toggle()
+    {
+        setWelcome(prevWelcome => !prevWelcome)
+    }
+    console.log(welcome)
     return(
-        <Welcome />
+
+        welcome ? <Welcome toggle={toggle} /> : <Card />
     )
 }
