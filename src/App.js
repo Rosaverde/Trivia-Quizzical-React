b93e4588d25f316f38b4db85accc64906aa88e11
Rosaverde <rosaverde.ag@gmail.com>
2023-07-14 23:36:56 +0000

    Prepared bord for question rendering

--- src/App.js
+++ src/App.js
@@ -2,17 +2,13 @@ import React from "react";
 import Welcome from "./components/Welcome";
 import Card from "./components/Card";
 
-export default function App()
-{
+export default function App() {
     const [welcome, setWelcome] = React.useState(true)
 
-    function toggle()
-    {
+    function toggle() {
         setWelcome(prevWelcome => !prevWelcome)
     }
-    console.log(welcome)
-    return(
-
+    return (
         welcome ? <Welcome toggle={toggle} /> : <Card />
     )
 }
