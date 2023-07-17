2c9f2d0ef56e4352380268bcabf5cf00293c3845
Rosaverde <rosaverde.ag@gmail.com>
2023-07-17 23:19:46 +0000

    Basic question layout

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,6 +2,21 @@ import React from "react"
 
 export default function Question() {
     return (
-        <h1> Test</h1>
+        <form className="question-form">
+            <h2 className="question-text">Question</h2>
+            <input type="radio" id="value1" />
+            <label htmlFor="value1">Value 1</label>
+            <br/>
+            <input type="radio" id="value2" />
+            <label htmlFor="value2">Value 2</label>
+            <br/>
+            <input type="radio" id="value3" />
+            <label htmlFor="value3">Value 3</label>
+            <br/>
+            <input type="radio" id="value4" />
+            <label htmlFor="value4">Value 4</label>
+            <br/>
+
+        </form>
     )
 }
