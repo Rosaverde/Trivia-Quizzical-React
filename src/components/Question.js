31802deb1ae5d4bac467bcd74d3461c45acba17f
Rosaverde <rosaverde.ag@gmail.com>
2023-07-19 23:29:40 +0000

    Fixed ktml-entities and question styling

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,4 +1,5 @@
 import React from "react"
+import {decode} from "html-entities"
 
 export default function Question(props) {
 
@@ -6,22 +7,21 @@ export default function Question(props) {
 
     return (
         <form className="question-form">
-            <h5 className="question-text">{props.question}</h5>
+            <h5 className="question-text">{decode(props.question)}</h5>
             <div className="answers">
-                <input type="radio" id="value1" />
-                <label htmlFor="value1">{shuffledAnswers[0]}</label>
+                <input type="radio" name="test" id={shuffledAnswers[0]} />
+                <label htmlFor={shuffledAnswers[0]}>{decode(shuffledAnswers[0])}</label>
                 <br />
-                <input type="radio" id="value2" />
-                <label htmlFor="value2">{shuffledAnswers[1]}</label>
+                <input type="radio" name="test" id={shuffledAnswers[1]} />
+                <label htmlFor={shuffledAnswers[1]}>{decode(shuffledAnswers[1])}</label>
                 <br />
-                <input type="radio" id="value3" />
-                <label htmlFor="value3">{shuffledAnswers[2]}</label>
+                <input type="radio" name="test" id={shuffledAnswers[2]} />
+                <label htmlFor={shuffledAnswers[2]}>{decode(shuffledAnswers[2])}</label>
                 <br />
-                <input type="radio" id="value4" />
-                <label htmlFor="value4">{shuffledAnswers[3]}</label>
+                <input type="radio" name="test" id={shuffledAnswers[3]} />
+                <label htmlFor={shuffledAnswers[3]}>{decode(shuffledAnswers[3])}</label>
                 <br />
             </div>
-
         </form>
     )
 }
